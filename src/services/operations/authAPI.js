0ac3826a9fccf0fb829ac5de95eeb908636c0dd9
Student Aman4580 <amanverma7124@gmail.com>
2024-07-16 17:25:27 +0530

    commit

--- src/services/operations/authAPI.js
+++ src/services/operations/authAPI.js
@@ -84,7 +84,7 @@ export function login(email, password, navigate) {
       navigate("/dashboard/my-profile")
     } catch (error) {
       console.log("LOGIN API ERROR............", error)
-      toast.error("Login Failed")
+      toast.error("Login Failed due to some reason")
     }
     dispatch(setLoading(false))
     toast.dismiss(toastId)
